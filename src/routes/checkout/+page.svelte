f63fb7b8190e6fc38de36b7bc7286b75b16d9df6
MyHuong505 <myhuong.nn@gmail.com>
2023-08-23 22:18:35 +0700

    update

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -1,1 +1,67 @@
-<p>Check out</p>
+<script>
+
+</script>
+
+
+<div class="bg-gray-100 w-full h-full flex justify-center">
+    <div class="w-3/6 bg-white overflow-auto my-4 rounded">
+        <div class="text-stone-700 mx-4 my-2 pb-2 pt-4 text-md font-extrabold">Shipping Address</div>
+    <div class="flex flex-row my-4">
+        <div class="flex flex-col w-1/2">
+            <span class="text-stone-500 text-sm mx-4 mb-1">*First Name</span>
+            <input type="text" class="mx-4 border rounded py-1 pl-1" />
+        </div>
+        <div class="flex flex-col w-1/2">
+            <span class="text-stone-500 text-sm mx-4 mb-1">*Last Name</span>
+            <input type="text" class="mx-4 border rounded py-1 pl-1" />
+        </div>
+    </div>
+    <div class="flex flex-row my-4">
+        <div class="flex flex-col w-1/2">
+            <span class="text-stone-500 text-sm mx-4 mb-1">*Address line 1</span>
+            <input type="text" class="mx-4 border rounded py-1 pl-1" />
+        </div>
+        <div class="flex flex-col w-1/2">
+            <span class="text-stone-500 text-sm mx-4 mb-1">Address line 2 (optional)</span>
+            <input type="text" class="mx-4 border rounded py-1 pl-1" />
+        </div>
+    </div>
+    <div class="flex flex-row my-4">
+        <div class="flex flex-col w-1/3">
+            <span class="text-stone-500 text-sm mx-4 mb-1">*Country/Region</span>
+            <input type="text" class="mx-4 border rounded py-1 pl-1" />
+        </div>
+        <div class="flex flex-col w-1/3">
+            <span class="text-stone-500 text-sm mx-4 mb-1">State/County</span>
+            <input type="text" class="mx-4 border rounded py-1 pl-1" />
+        </div>
+        <div class="flex flex-col w-1/3">
+            <span class="text-stone-500 text-sm mx-4 mb-1">*City</span>
+            <input type="text" class="mx-4 border rounded py-1 pl-1" />
+        </div>
+    </div>
+    <div class="flex flex-row my-4">
+        <div class="flex flex-col w-1/2">
+            <span class="text-stone-500 text-sm mx-4 mb-1">*Zip/Postal Code</span>
+            <input type="text" class="mx-4 border rounded py-1 pl-1" />
+        </div>
+        <div class="flex flex-col w-1/2 relative">
+            <span class="text-stone-500 text-sm mx-4 mb-1">*Phone Number</span>
+            <input type="text" class="mx-4 border rounded py-1 pl-1" />
+        </div>
+    </div>
+    <div class="flex items-center justify-center">
+        <button class="w-1/5 bg-primary text-white py-2 rounded text-sm hover:bg-secondary">
+            Save
+        </button>
+    </div>
+    
+
+        
+    </div>
+    <div class="w-1/5 h-1/5 py-2 bg-white m-4 sticky top-4 rounded">
+        <p class="text-stone-700 border-b mx-4 py-4 text-md font-extrabold">Order Summary</p>
+        <p class="py-2 px-4 text-stone-500 text-sm text-left"> Total:
+           
+    </div>
+</div>
