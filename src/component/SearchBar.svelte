bbac5847d8ce5c247c5a6e78b543a363ee3a5d07
MyHuong505 <myhuong.nn@gmail.com>
2023-10-01 23:27:01 +0700

    update

--- src/component/SearchBar.svelte
+++ src/component/SearchBar.svelte
@@ -1,5 +1,15 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
 
+  let searchTerm = '';
+  let searchQuery = '';
+
+  const dispatch = createEventDispatcher();
+
+  function handleSearch() {
+    searchQuery = searchTerm; // Lưu giá trị tìm kiếm vào biến tính toán
+    dispatch('search', searchTerm);
+  }
 </script>
 
 <div class="flex items-center justify-center">
@@ -7,10 +17,11 @@
     type="text"
     placeholder="Search..."
     class="w-1/3 border rounded-l py-2 px-3 focus:outline-none border-primary"
-
+    bind:value={searchTerm}
   />
   <button
     class="border border-primary border-l-0 bg-primary text-white rounded-r py-2 px-4 hover:bg-primary-dark"
+    on:click={handleSearch}
   >
     Search
   </button>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -6,36 +6,62 @@
   import { onMount } from 'svelte';
 
   let products = [];
-  let categories = []
+  let categories = [];
+  let searchTerm = ''; // Biến state để lưu giá trị tìm kiếm
 
-  onMount(()=> {
+  onMount(() => {
     fetchProduct();
-    fetchCategory()
-  })
+    fetchCategory();
+  });
 
   async function fetchProduct(categoryId) {
-    let response 
-    if (categoryId) {
-      response = await fetch(`http://localhost:4000/products?categoryId=${categoryId}`);
-    } else {
-      response = await fetch('http://localhost:4000/products');
-    } 
-    products = await response.json();
+    try {
+      let url = 'http://localhost:4000/products';
+      if (categoryId) {
+        url += `?categoryId=${categoryId}`;
+      }
+      if (searchTerm) {
+        url += `&searchTerm=${searchTerm}`;
+      }
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Lỗi khi tìm kiếm sản phẩm');
+      }
+      const data = await response.json();
+      products = data;
+    } catch (error) {
+      console.error('Lỗi khi tìm kiếm sản phẩm:', error);
+    }
   }
 
   async function fetchCategory() {
-    const res = await fetch('http://localhost:4000/categories');
-    categories = await res.json();
+    try {
+      const res = await fetch('http://localhost:4000/categories');
+      if (!res.ok) {
+        throw new Error('Lỗi khi tải danh mục');
+      }
+      categories = await res.json();
+    } catch (error) {
+      console.error('Lỗi khi tải danh mục:', error);
+    }
   }
 
-  
-
+  function handleSearch(event) {
+    searchTerm = event.detail; // Cập nhật giá trị tìm kiếm
+    fetchProduct(null, searchTerm);
+  }
 </script>
 
 <div class="my-8">
-  <SearchBar />
+  <!-- SearchBar với sự kiện search -->
+  <SearchBar on:search={handleSearch} />
+
+  <!-- Filter -->
   <Filter {categories} {fetchProduct} />
+
+  <!-- ProductCard -->
   <ProductCard {products} />
 </div>
 
+<!-- Footer -->
 <Footer />
