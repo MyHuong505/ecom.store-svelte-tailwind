7431f431823369c8889b700800a2ca8f032ec4b7
MeiXiang <myhuong.nn@gmail.com>
2024-01-07 18:06:38 +0700

    Adjusted Footer

--- src/component/Footer.svelte
+++ src/component/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="w-full bg-stone-200 right-0 flex justify-center items-center flex-col">
-    <div class="flex items-center justify-between mt-8 pb-8">
+    <div class="flex items-center justify-evenly mt-8 pb-8 flex-wrap space-y-4">
         <div class="w-52 flex flex-col items-center">
             <i class="fas fa-gift text-2xl text-gray-600"></i>
             <p class="text-sm text-gray-700">Rich in Variety</p>
@@ -26,29 +26,30 @@
             <p class="text-gray-600 text-xs">7-day Satisfaction Guarantee</p>
         </div>
     </div>
-
-    <div class="flex justify-between w-[1000px] mt-4 border-b border-gray-300 pb-8">
-
-        <div class="flex flex-col">
-            <p class="text-sm text-gray-700 my-2">Subscribe to Our Newsletter & Get Exclusive Offers</p>
-            <div class="flex">
-                <input
-                    type="email"
-                    placeholder="Your Email Address"
-                    class="w-64 px-4 py-2 text-sm rounded-l border border-gray-300 focus:outline-none"
-                />
-                <button
-                    class="px-4 py-2 text-sm bg-primary text-white rounded-r focus:outline-none"
-                >
-                    Subscribe
-                </button>
+    <div class="flex mt-4 pb-8 space-x-8 flex-wrap space-y-2 justify-center">
+        <div class="flex">
+            
+            <div class="flex flex-col">
+                <p class="text-sm text-gray-700 my-2">Subscribe to Our Newsletter & Get Exclusive Offers</p>
+                <div class="flex">
+                    <input
+                        type="email"
+                        placeholder="Your Email Address"
+                        class="w-64 px-4 py-2 text-sm rounded-l border border-gray-300 focus:outline-none"
+                    />
+                    <button
+                        class="px-4 py-2 text-sm bg-primary text-white rounded-r focus:outline-none"
+                    >
+                        Subscribe
+                    </button>
+                </div>
             </div>
         </div>
-
-
+        
+        
         <div class="flex flex-col">
             <p class="text-sm text-gray-700 my-2">Join Our Community</p>
-                <div class="flex items-center gap-6">
+            <div class="flex items-center gap-6">
                 <i class="fab fa-facebook text-xl text-gray-700"></i>
                 <i class="fab fa-instagram text-xl text-gray-700"></i>
                 <i class="fab fa-tiktok text-xl text-gray-700"></i>
@@ -56,91 +57,92 @@
                 <i class="fab fa-youtube text-xl text-gray-700"></i>
                 <i class="fab fa-twitter text-xl text-gray-700"></i>
                 <i class="fab fa-tumblr text-xl text-gray-700"></i>
+            </div>
+        </div>
+        
+        <div class="flex flex-col">
+            <p class="text-sm text-gray-700 my-2">Our Mobile Apps</p>
+            
+            <div class="flex gap-4">
+                <div class="bg-stone-800 w-32 flex items-center p-2 px-4 rounded">
+                    <i class="fab fa-apple text-stone-200"></i>
+                    <p class="text-stone-200 text-xs pl-2">Apple Store</p>
                 </div>
-        </div>
-    <div class="flex flex-col">
-        <p class="text-sm text-gray-700 my-2">Our Mobile Apps</p>
-
-        <div class="flex gap-4">
-            <div class="bg-stone-800 w-32 flex items-center p-2 px-4 rounded">
-                <i class="fab fa-apple text-stone-200"></i>
-                <p class="text-stone-200 text-xs pl-2">Apple Store</p>
+                <div class="bg-stone-800  w-32 flex items-center p-2 px-4 rounded">
+                    <i class="fab fa-google-play text-stone-200"></i>
+                    <p class="text-stone-200 text-xs pl-2">Google Play</p>
+                </div>
             </div>
-            <div class="bg-stone-800  w-32 flex items-center p-2 px-4 rounded">
-                <i class="fab fa-google-play text-stone-200"></i>
-                <p class="text-stone-200 text-xs pl-2">Google Play</p>
-            </div>
-        </div>
 
-    </div>    
+        </div>    
     </div>
     
-<div class="w-[1000px] py-8 flex items-center justify-center">
-    <div class="flex w-[1000px] justify-between gap-4">
-
-        <div class="">
-            <h2 class="text-sm text-gray-700">Company Info</h2>
-            <ul class="text-gray-600 text-xs">
-                <li class="my-1">About</li>
-                <li class="my-1">Imprint</li>
-                <li class="my-1">Customer Service</li>
-                <li class="my-1">How to contact us</li>
-                <li class="my-1">How to return</li>
-            </ul>
-        </div>
-
-        <div class="">
-            <h2 class="text-sm text-gray-700">Accessibility</h2>
-            <ul class="text-gray-600 text-xs">
-                <li class="my-1">How to track</li>
-                <li class="my-1">How to order</li>
-            </ul>
-        </div>
-
-        <div class="">
-            <h2 class="text-sm text-gray-700">Our Guarantee</h2>
-            <ul class="text-gray-600 text-xs">
-                <li class="my-1">Company Policies</li>
-                <li class="my-1">Return Policy</li>
-                <li class="my-1">Privacy Policy</li>
-                <li class="my-1">Terms of Use</li>
-                <li class="my-1">Cookie Notice</li>
-                <li class="my-1">Intellectual Property Policy</li>
-            </ul>
-        </div>
-
-        <div class="">
-            <h2 class="text-sm text-gray-700">All you need to know</h2>
-            <ul class="text-gray-600 text-xs">
-                <li class="my-1">Payment & Methods</li>
-                <li class="my-1">Payment Methods</li>
-                <li class="my-1">Estimated Delivery Time</li>
-            </ul>
-        </div>
-
-        <div class="">
-            <h2 class="text-sm text-gray-700">Other Business</h2>
-            <ul class="text-gray-600 text-xs">
-                <li class="my-1">Affiliate Program</li>
-                <li class="my-1">Recruiting Designers</li>
-            </ul>
+    <div class="py-8 flex items-center justify-center">
+        <div class="flex justify-between gap-4">
+            
+            <div class="">
+                <h2 class="text-sm text-gray-700">Company Info</h2>
+                <ul class="text-gray-600 text-xs">
+                    <li class="my-1">About</li>
+                    <li class="my-1">Imprint</li>
+                    <li class="my-1">Customer Service</li>
+                    <li class="my-1">How to contact us</li>
+                    <li class="my-1">How to return</li>
+                </ul>
+            </div>
+            
+            <div class="">
+                <h2 class="text-sm text-gray-700">Accessibility</h2>
+                <ul class="text-gray-600 text-xs">
+                    <li class="my-1">How to track</li>
+                    <li class="my-1">How to order</li>
+                </ul>
+            </div>
+            
+            <div class="">
+                <h2 class="text-sm text-gray-700">Our Guarantee</h2>
+                <ul class="text-gray-600 text-xs">
+                    <li class="my-1">Company Policies</li>
+                    <li class="my-1">Return Policy</li>
+                    <li class="my-1">Privacy Policy</li>
+                    <li class="my-1">Terms of Use</li>
+                    <li class="my-1">Cookie Notice</li>
+                    <li class="my-1">Intellectual Property Policy</li>
+                </ul>
+            </div>
+            
+            <div class="">
+                <h2 class="text-sm text-gray-700">All you need to know</h2>
+                <ul class="text-gray-600 text-xs">
+                    <li class="my-1">Payment & Methods</li>
+                    <li class="my-1">Payment Methods</li>
+                    <li class="my-1">Estimated Delivery Time</li>
+                </ul>
+            </div>
+            
+            <div class="">
+                <h2 class="text-sm text-gray-700">Other Business</h2>
+                <ul class="text-gray-600 text-xs">
+                    <li class="my-1">Affiliate Program</li>
+                    <li class="my-1">Recruiting Designers</li>
+                </ul>
+            </div>
         </div>
     </div>
-</div>
-
-
-
+    
+    
+    
     <div class="flex py-3 mb-8 w-full bg-stone-800 justify-center">
         <div class="flex items-center justify-between w-[1000px]">
-        <p class="text-stone-300 text-xs">@Copy right 2023</p>
-        <ul class="flex gap-4">
-            <li class="text-stone-300 text-xs">Country & Region: Singapore</li>
-            <li class="text-stone-300 text-xs">Vietnam</li>
-            <li class="text-stone-300 text-xs">Australia</li>
-            <li class="text-stone-300 text-xs">America</li>
-        </ul>
+            <p class="text-stone-300 text-xs">@Copy right 2023</p>
+            <ul class="gap-4 hidden md:flex">
+                <li class="text-stone-300 text-xs">Country & Region: Singapore</li>
+                <li class="text-stone-300 text-xs">Vietnam</li>
+                <li class="text-stone-300 text-xs">Australia</li>
+                <li class="text-stone-300 text-xs">America</li>
+            </ul>
         </div>
-
+        
     </div>
-
+    
 </footer>
